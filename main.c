efcf2eca09309f6e995b797a3dc34bb01a7aee0e
Boris Gezkovski <bgezkov@pm.me>
2026-02-11 17:13:44 +0200

    Finished chapter 2.6

--- main.c
+++ main.c
@@ -3,54 +3,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define R 3
-#define C 6
-int change_value(int *input);
-
 void cleanup_pointer(void **p);
 
 void check_pointer(void *p);
 
 int main(void){
+    int size;
+    char str[64];
+    char *new_str = NULL;
 
-    int **two_d_arr;
+    strcpy(str, "Hello");
+    size = strlen(str);
 
-    two_d_arr = malloc(sizeof(int *) * R);
-    check_pointer((void *) two_d_arr);
+    new_str = malloc(sizeof(char) * (size+1));
+    check_pointer((void*) new_str);
 
-    for(int i = 0; i < R; i++){
-        two_d_arr[i] = malloc(sizeof(int) * C);
-    }
+    strcpy(new_str, str);
 
-    for(int r = 0; r < R; r++){
-        for(int c = 0; c < C; c++){
-            two_d_arr[r][c] = r * c;
-        }
-    }
-    
-    for(int r = 0; r < R; r++){
-        for(int c = 0; c < C; c++){
-            printf("%d ", two_d_arr[r][c]);
-        }
-        printf("\n");
-    }
+    printf("%s %s\n", str, new_str);
+    strcat(str, " There");
 
-    cleanup_pointer((void**)&two_d_arr);
+    printf("%s\n", str);
+
+    cleanup_pointer((void**)&new_str);
 
     return 0;
 } 
-
-int change_value(int *input){
-    int val;
-    val = *input;
-
-    if (val < 100){
-        *input = 100;
-    }else{
-        *input = val * 2;
-    }
-    return val;
-}
 
 void cleanup_pointer(void **p){
     free(*p);
