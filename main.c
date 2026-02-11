bb22f4490d34795d25ffd847d3ba1b15c5ae8387
Boris Gezkovski <bgezkov@pm.me>
2026-02-11 13:13:57 +0200

    Dynamically alocated array

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int change_value(int *input);
 
-int main(void){
-    
-    int x = 101;
-    int y = change_value(&x);
+void cleanup_pointer(void **p);
 
-    printf("x = %d | y = %d\n", x, y);
+void check_pointer(void *p);
+
+int main(void){
+
+    int *arr;
+    int s_arr[20];
+
+    arr = malloc(sizeof(int) * 20);
+    check_pointer((void*) arr);
+    for(int i=0; i < 20; i++){
+        s_arr[i] = i;
+        arr[i] = i;
+    }
+
+    printf("%d %d \n", s_arr[3], arr[3]);
 
     return 0;
 } 
@@ -24,4 +36,16 @@ int change_value(int *input){
         *input = val * 2;
     }
     return val;
+}
+
+void cleanup_pointer(void **p){
+    free(*p);
+    *p = NULL;
+}
+
+void check_pointer(void *p){
+	if(p == NULL){
+		printf("Pointer is NULL!\n");
+		exit(1);
+	}
 }
